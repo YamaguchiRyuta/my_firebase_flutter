e09e130cf668799c08ad0c14f0c61a5dfa6fea0a
YamaguchiRyuta <steven_siro_vai@hotmail.co.jp>
2021-08-05 17:56:31 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,128 @@
+# my_firebase
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+# 環境構築でのメモ
+
+- [vulkan Runtime](https://vulkan.lunarg.com/sdk/home) が別途必要だった。
+- Hyper-VとHAXMの共存はできない。
+- Intel Virtualization Technologyも有効にする。
+
+# 開発手順
+- Android StudioでFlutterプロジェクトを新規作成
+- [pubspec.yamlにfirebase.coreを追加](https://firebase.flutter.dev/docs/overview/#installation)
+
+  ```bash
+  # Packageの追加
+  flutter pub add firebase_core
+  flutter pub add firebase_analytics
+  flutter pub add firebase_auth
+  flutter pub add cloud_firestore
+  # Packageの取得(上記コマンドを実行した場合は不要)
+  #  flutter pub get
+  ```
+
+  ```dart
+  import 'package:firebase_core/firebase_core.dart';
+  import 'package:firebase_auth/firebase_auth.dart';
+  import 'package:cloud_firestore/cloud_firestore.dart';
+  ```
+
+- [web/index.htmlにSDKを設定](https://firebase.flutter.dev/docs/installation/web)
+  追記内容はFirebaseプロジェクトのマイアプリにウェブアプリを追加し、
+  「プロジェクトの設定」から確認できる。
+
+  ```html
+  <!-- The core Firebase JS SDK is always required and must be listed first -->
+  <script src="https://www.gstatic.com/firebasejs/8.6.1/firebase-app.js"></script>
+  
+  <!-- TODO: Add SDKs for Firebase products that you want to use
+       https://firebase.google.com/docs/web/setup#available-libraries -->
+  <script src="https://www.gstatic.com/firebasejs/8.6.1/firebase-analytics.js"></script>
+
+  <script src="https://www.gstatic.com/firebasejs/8.6.1/firebase-auth.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.6.1/firebase-firestore.js"></script>
+
+  <script>
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    var firebaseConfig = {
+  /*
+      apiKey: "API_KEY",
+      authDomain: "PROJECT_ID.firebaseapp.com",
+      databaseURL: "https://PROJECT_ID.firebaseio.com",
+      projectId: "PROJECT_ID",
+      storageBucket: "PROJECT_ID.appspot.com",
+      messagingSenderId: "SENDER_ID",
+      appId: "APP_ID",
+      measurementId: "G-MEASUREMENT_ID",
+  */
+    };
+  
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  </script>
+  ```
+
+- [FlutterFireを初期化するようmain.dartを編集](https://firebase.flutter.dev/docs/overview/#initializing-flutterfire)
+  FutureBuilder経由でMyAppを渡す。
+  firebaseのauth, storeなどを実装
+# デプロイ手順
+- Firebase CLIをインストール
+
+  ```bash
+  npm install -g firebase-tools
+
+  firebase login
+  ```
+
+- Firebaseプロジェクトとして初期化
+  publicディレクトリの指定では build/web を指定すること
+  色々聞かれるが、CLIのメッセージを見ればなんとかなる
+  ```bash
+  firebase init
+  ```
+  
+- FlutterでWebを有効化していないならする
+  ```bash
+  flutter channel stable
+  flutter upgrade
+  flutter config --enable-web
+  # com.exampleから変更していれば--orgオプションは省略可
+  flutter create --org package_name .
+  ```
+- FlutterをWeb用にビルド
+  通常はCanvas要素としてレンダリングされる。
+  フォントを追加しないと漢字が中国語っぽいフォントになってしまう
+  ```bash
+  flutter build web
+  # flutter build web --web-renderer canvaskit
+  ```
+
+  htmlでビルドする場合はこっち。
+  漢字のフォントは直るが、Firefoxだと微妙にレイアウトがズレたりするっぽい。
+  (FloatingActionButtonにhoverした時など)
+  ```bash
+  flutter build web --web-renderer html
+  ```
+  
+  デプロイ実行
+  ```bash
+  firebase deploy
+  ```
+  
+  [Firebaseドキュメント](https://firebase.google.com/docs/hosting?hl=ja)
---
+++ lib/main.dart
@@ -0,0 +1,224 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
+
+  FirebaseAuth.instance.authStateChanges().listen((User? user) {
+    if (user == null) {
+      print('User is currently signed out!');
+    } else {
+      print('User is signed in!');
+    }
+  });
+}
+
+/// We are using a StatefulWidget such that we only create the [Future] once,
+/// no matter how many times our widget rebuild.
+/// If we used a [StatelessWidget], in the event where [App] is rebuilt, that
+/// would re-initialize FlutterFire and make our application re-enter loading state,
+/// which is undesired.
+class App extends StatefulWidget {
+  // Create the initialization Future outside of `build`:
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return SomethingWentWrong();
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Loading();
+      },
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  void _store() async {
+    FirebaseFirestore.instance
+        .collection('test_collection_1')
+        .doc('test_document_1')
+        .get()
+        .then((ref) {
+      print(ref.get("test_key"));
+    });
+  }
+
+  void _login() async {
+    try {
+      // 認証
+      (await FirebaseAuth.instance.signInWithEmailAndPassword(
+              email: 'ryuta.yamaguchi@takacom.co.jp', password: 'xxxxxx'))
+          .user;
+    } catch (e, s) {
+      print('$e: $s');
+      // 登録
+      final User? user = (await FirebaseAuth.instance
+              .createUserWithEmailAndPassword(
+                  email: 'ryuta.yamaguchi@takacom.co.jp', password: 'xxxxxx'))
+          .user;
+      print("登録: $user");
+      return;
+    }
+    print("もう登録済み");
+  }
+
+  void _logout() async {}
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            TextButton(
+              onPressed: _login,
+              child: Text("ログイン"),
+            ),
+            TextButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+              },
+              child: Text("ログアウト"),
+            ),
+            TextButton(
+              onPressed: _store,
+              child: Text("取得"),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class Loading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(body: Center(child: Text("Loading..."))));
+  }
+}
+
+class SomethingWentWrong extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(body: Center(child: Text("SomethingWentWrong!"))));
+  }
+}
